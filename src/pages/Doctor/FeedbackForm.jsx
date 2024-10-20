5f7eb8e11941ba0a9a380f69b7ad7949c1e8d203
unknown <Ali-baqar13>
2024-10-21 04:16:23 +0500

    fourthOne

--- src/pages/Doctor/FeedbackForm.jsx
+++ src/pages/Doctor/FeedbackForm.jsx
@@ -1,13 +1,56 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import {AiFillStar} from 'react-icons/ai'
+import { toast } from "react-toastify";
+import {BASE_URL} from "../../../config";
+import { useParams } from 'react-router-dom';
+import { authContext } from '../../context/AuthContext';
+import HashLoader from 'react-spinners/HashLoader';
+
 const FeedbackForm = () => {
     const [Rating , setRating]=useState(0)
     const [hover , sethover]=useState(0)
     const [Review , setReview]=useState('')
+    const { token } = useContext(authContext);
+    const [loading, setLoading]=useState(false)
+    const {doctorId}=useParams()
+   
     const handleSubmitReview =async e=>{
         e.preventDefault()
         //........API..................
+        setLoading(true)
+        
+        try{
+            if ( !Review || !Rating){setLoading(false)
+                toast.error("error")
+            }
+            const res= await fetch(`${BASE_URL}/doctor/${doctorId}/reviews`,{
+                method:"post",
+                headers:{
+                Authorization:`Bearer ${token}`,
+                "Content-Type":"application/json"},
+                body: JSON.stringify({Review, Rating})
+            })
+           
+            const result = await res.json();
+            if(!res.ok){
+                throw new Error(result.message)
+            }
+            setLoading(false)
+            toast.success("woooow")
+            
+            
+
+        }catch(e){
+            
+            setLoading(false)
+            toast.error("errored")
     }
+
+    }
+   
+
+    
+    
   return (
     <>
     <form action='' >
@@ -31,7 +74,7 @@ const FeedbackForm = () => {
             <h3 className='text-headingColor text-[16px] leading-6 font-semibold '>Show Your FeedBack Here</h3>
 
             <textarea className='border border-solid border-[#0066ff34] focus:outline outline-primaryColor w-full px-4 py-3 rounded-md' placeholder='write you feedback' onChange={(e)=>setReview(e.target.value)} rows='5'></textarea>
-            <button type='submit' onClick={handleSubmitReview} className='btn'>Add</button>
+            <button type='submit' onClick={handleSubmitReview} className='btn'>{loading? <HashLoader size={25} color='white'/>:'submit Review'}</button>
         </div>
     </form>
     
